cc565198bd67f84c9a9b1fa83ae07d822943219b
Dmitry Ishkov <dimanrussian@gmail.com>
2012-12-26 13:49:49 +0400

    initial commit

---
+++ src/Model/Coordinate.java
@@ -0,0 +1,11 @@
+package Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: TheRusskiy
+ * Date: 26.12.12
+ * Time: 13:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class Coordinate {
+}
